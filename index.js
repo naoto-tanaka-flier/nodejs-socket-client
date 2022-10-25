1aff67a0594c2c4590289838c0bac2966a020b01
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-10-25 23:23:52 +0900

    socket.ioのCDNを追加してからソケット通信を開始するように修正

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const socket = io.connect('http://localhost:6002');
-// ソケット確立
-socket.on('connect', function() {
-  console.log('socket connected')
-});
-// ソケット確立に失敗
-socket.on("connect_error", () => {
-  console.log('socket connect failed...')
-});
-// サーバーからメッセージを受信
-socket.on('message', function(data) {
-  location.reload()
-})
+const s = document.createElement('script');
+s.setAttribute('src', 'https://cdn.socket.io/4.5.3/socket.io.min.js');
+s.setAttribute('integrity', 'sha384-WPFUvHkB1aHA5TDSZi6xtDgkF0wXJcIIxXhC6h8OT8EH3fC5PWro5pWJ1THjcfEi')
+s.setAttribute('crossorigin', 'anonymous')
+s.onload = function(){
+  const socket = io.connect('http://localhost:6002');
+  // ソケット確立
+  socket.on('connect', function() {
+    console.log('socket connected')
+  });
+  // ソケット確立に失敗
+  socket.on("connect_error", () => {
+    console.log('socket connect failed...')
+  });
+  // サーバーからメッセージを受信
+  socket.on('message', function(data) {
+    location.reload()
+  })
+};
+document.body.appendChild(s);
